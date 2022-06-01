e47e34914a6a55616777d433b57a0d6381455246
NicoMadd <nicomad.17@gmail.com>
2022-06-01 02:00:33 -0300

    - Criterio de Seleccion ahora se compone de una AF

--- src/Criterios/Seleccion/CriterioSeleccion.py
+++ src/Criterios/Seleccion/CriterioSeleccion.py
@@ -18,10 +18,14 @@ class CriterioSeleccion:
     # fraccionamiento: float es la fraccion de individuos que se eligen del subgrupo final
     def __init__(self,  fraccionamiento=0.1) -> None:
         self.fraccionamiento: float = fraccionamiento
+        self.funcionAptitud: FuncionAptitud = None
 
     @abstractmethod
     def seleccionar(self, poblacion: DataFrame) -> DataFrame:
         return poblacion
+
+    def setFuncionAptitud(self, funcionAptitud: FuncionAptitud) -> None:
+        self.funcionAptitud = funcionAptitud
 
 
 class Torneo(CriterioSeleccion):
@@ -40,7 +44,7 @@ class Ranking(CriterioSeleccion):
 
     def seleccionar(self, subgrupo: DataFrame) -> DataFrame:
         # Ordena al subgrupo por el valor de la funcion aptitud
-        subgrupo["aptitud"] = FuncionAptitud().evaluar(subgrupo)
+        subgrupo["aptitud"] = self.funcionAptitud.evaluar(subgrupo)
         subgrupo.sort_values(by=['aptitud'], ascending=False, inplace=True)
         # Selecciona el subgrupo final priorizando los mejores
         tamanio = math.floor(len(subgrupo)*self.fraccionamiento)
