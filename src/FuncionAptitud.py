3d64923bf82d18b410cb6674ea9ae1e0e05a0859
NicoMadd <nicomad.17@gmail.com>
2022-06-01 01:59:41 -0300

    - Se estandariza FA con None - Se deja una normal en FA de ejemplo

--- src/FuncionAptitud.py
+++ src/FuncionAptitud.py
@@ -1,38 +1,34 @@
 # Esta clase se encarga de construir mediante parametros, ya sean seteados para testear o obtenidos del usuario, para armar la funcion aptitud del calculo de atraccion a un libro/individuo.
 
-from pandas import Series
+import numpy as np
+from pandas import DataFrame, Series
 
 
 class FuncionAptitud:
-    def __init__(self):
-        self.autor = None
-        self.titulo = None
-        self.genero = None
-        self.calificacion = None
-        self.publicador = None
-        self.porcentaje_aprobacion = None
-
-    def set_autor(self, autor):
+    def __init__(self, autor=None, titulo=None, genero=None, calificacion=None, publicador=None,
+                 porcentaje_aprobacion=None, precio=None, cantidad_paginas=None, fecha_publicacion=None):
         self.autor = autor
-
-    def set_titulo(self, titulo):
         self.titulo = titulo
-
-    def set_genero(self, genero):
         self.genero = genero
-
-    def set_calificacion(self, calificacion):
         self.calificacion = calificacion
-
-    def set_publicador(self, publicador):
         self.publicador = publicador
-
-    def set_porcentaje_aprobacion(self, porcentaje_aprobacion):
         self.porcentaje_aprobacion = porcentaje_aprobacion
+        self.precio = precio
+        self.cantidad_paginas = cantidad_paginas
+        self.fecha_publicacion = fecha_publicacion
+        self.once = True
 
-    def evaluar(self, individuo: Series) -> float:
-        # contar vocales en titulo
-        # return individuo["titulo"].str.count("a") + individuo["titulo"].str.count("e") + individuo["titulo"].str.count("i") + individuo["titulo"].str.count("o") + individuo["titulo"].str.count("u")
+    def evaluar(self, individuo: DataFrame) -> Series:
+        """
+        Evalua la funcion aptitud de un individuo.
+        """
 
-        # el titulo mas largo
-        return individuo["titulo"].str.len()
+        # return random float series
+        return np.random.normal(0, 100, individuo.shape[0])
+
+        # aptitud = 0
+        # if(self.once == True):
+        #     print(individuo)
+        #     print(aptitud)
+        # self.once = False
+        # return aptitud
